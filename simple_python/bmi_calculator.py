829ca728660356ad3fcce74fd392ad752eb8a7ed
Vangar83 <60311476+Vangar83@users.noreply.github.com>
2020-01-26 09:14:23 +0000

    Add files via upload

---
+++ simple_python/bmi_calculator.py
@@ -0,0 +1,17 @@
+#BMI calculator
+
+name = 'Vangar'
+height_m = 1.83
+weight_kg = 75
+bmi = weight_kg / (height_m **2)
+
+print (bmi)
+
+if bmi <25:
+    print (name)
+    print ('Vangar is not overweight')
+
+else:
+    print (name)
+    print ('Vangar is overweight')
+    
