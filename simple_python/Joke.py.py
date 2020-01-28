70cf1c1ad1e0fb416b8d5d759ce715910e422c17
Vangar83 <60311476+Vangar83@users.noreply.github.com>
2020-01-28 20:27:06 +0000

    Add files via upload

---
+++ simple_python/Joke.py.py
@@ -0,0 +1,13 @@
+# Joke
+
+def tell (intro, punchline):
+    print ("Knock Knock")
+    print ("Who's there?")
+    print (intro)
+    print (intro + " who?")
+    print (punchline)
+
+  
+# call the function
+
+tell("Atch", "Sounds like you’ve got a cold!")
