3a594b2c544a38f3cc72a69d1637c86498d24efe
Vangar83 <60311476+Vangar83@users.noreply.github.com>
2020-01-28 20:47:53 +0000

    Add files via upload

---
+++ Data Analysis - Python/numpy and array.py
@@ -0,0 +1,18 @@
+import numpy as np
+import xlsxwriter
+m = np.array([20, 40, 50])
+n = 2
+print(m**n)
+
+
+table = [
+    [2,0],
+    [3,9]
+]
+
+print(table[0][1])
+
+
+for n in range(3):
+
+ print(n+1)
