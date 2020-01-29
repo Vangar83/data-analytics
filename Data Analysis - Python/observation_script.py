90756d2920a8412a247857cd7289517c1c80654c
Vangar83 <60311476+Vangar83@users.noreply.github.com>
2020-01-29 23:10:22 +0000

    Add files via upload

---
+++ Data Analysis - Python/observation_script.py
@@ -0,0 +1,25 @@
+import pandas as pd
+import numpy as np
+import xlrd
+
+client = pd.read_csv('client.csv', index_col = 'PRODUCT ID')
+pdata = pd.read_csv('InfoCSV.csv', index_col = 'PRODUCT ID')
+observe = pd.read_csv('Observation.csv', index_col = 'PRODUCT ID')
+
+
+
+df = observe.merge(client,on='PRODUCT ID', how = 'left', indicator=True)
+df = df.sort_values(by = ['_merge', 'Observation'],ascending = [True, False])
+df = df[df._merge !='both']
+
+df.drop(df.iloc[:, 1:14], inplace = True, axis = 1) 
+
+
+df1 = pd.merge(df,pdata[['PRODUCT NAME', 'CITY NAME','COUNTRY NAME']], on = 'PRODUCT ID', how = 'left')
+df1.dropna(subset=['PRODUCT NAME'],inplace=True)
+
+
+
+#print(df)
+
+df1.to_csv('Test-Succesful.csv', index=True)
